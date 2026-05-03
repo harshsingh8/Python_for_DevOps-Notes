3393bd888a401510c095ee8606028b75aae445e6
Harsh Kumar Singh <harshkumarsingh5658@gmail.com>
2026-05-03 17:13:01 +0530

    Python lessons

---
+++ Modules Examples/advanced_caclulator.py
@@ -0,0 +1,6 @@
+import calculator as basic_calc
+
+basic_calc
+
+print("\n")
+print(basic_calc.add())
---
+++ Modules Examples/calculator.py
@@ -0,0 +1,27 @@
+a = 10
+b = 5
+
+def add():
+    return (a+b)
+
+def sub():
+    return (a-b)
+
+def mul():
+    return (a*b)
+
+def div_int():
+    return (a//b)
+
+def modulo():
+    return (a%b)
+
+def div():
+    return (a/b)
+
+print("additon: ",add())
+print("Substraction: ",sub())
+print("division: ", div())
+print("division int : ", div_int())
+print("Multiplication: ",mul())
+print("modulo(remainder): ",modulo())
